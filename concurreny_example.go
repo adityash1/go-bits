0d5c346d9ad5a6f96593ac9ebeb9f4f9b2afe848
adityash1 <aditya167411@gmail.com>
2023-08-16 23:16:41 +0530

    learn: concurrency

---
+++ concurreny_example.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+type UserProfile struct {
+	ID       int
+	Comments []string
+	Likes    int
+	Friends  []int
+}
+
+type Response struct {
+	data any
+	err  error
+}
+
+func handleGetUserProfile(id int) (*UserProfile, error) {
+	var (
+		respch = make(chan Response, 3)
+		wg     = &sync.WaitGroup{}
+	)
+
+	go getComments(id, respch, wg)
+	go getLikes(id, respch, wg)
+	go getFriends(id, respch, wg)
+	wg.Add(3)
+	wg.Wait()
+
+	close(respch)
+
+	userProfile := &UserProfile{}
+
+	for resp := range respch {
+		if resp.err != nil {
+			return nil, resp.err
+		}
+		switch msg := resp.data.(type) {
+		case int:
+			userProfile.Likes = msg
+		case []int:
+			userProfile.Friends = msg
+		case []string:
+			userProfile.Comments = msg
+		}
+	}
+	return userProfile, nil
+}
+
+func getComments(id int, respch chan Response, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 200)
+	comments := []string{
+		"Hey! that was great",
+		"awesome",
+		"damn",
+	}
+	respch <- Response{
+		data: comments,
+		err:  nil,
+	}
+	wg.Done()
+}
+
+func getLikes(id int, respch chan Response, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 200)
+	respch <- Response{
+		data: 11,
+		err:  nil,
+	}
+	wg.Done()
+}
+
+func getFriends(id int, respch chan Response, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100)
+	friends := []int{
+		11, 34, 854, 455,
+	}
+	respch <- Response{
+		data: friends,
+		err:  nil,
+	}
+	wg.Done()
+}
+
+func main() {
+	start := time.Now()
+	UserProfile, err := handleGetUserProfile(10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(UserProfile)
+	fmt.Println("fetching user profile took ", time.Since(start))
+}
